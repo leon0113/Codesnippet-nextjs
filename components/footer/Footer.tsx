5a2b99ddae321053f498081b6732b3f30b0bda68
Leon <tahjib.leon399@gmail.com>
2024-04-02 03:37:39 +0600

    Footer updated

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <footer className="border-t border-black/5 py-5 mt-auto">
-            <p>A personal blog application for <a href="https://github.com/leon0113" target='_blank' className='font-bold'>Tahjib Hossain Leon</a></p>
+        <footer className="border-t border-black/5 py-5 mt-auto flex flex-col">
+            <small>A personal blog application for <a href="https://github.com/leon0113" target='_blank' className='font-bold hover:opacity-50'>Tahjib Hossain Leon</a></small>
             <small className="opacity-50 mt-5">
-                &copy; {new Date().getFullYear()} <a href="https://github.com/leon0113" target='_blank'><span className='font-bold text-base text-violet-400 opacity-100'>leon0113</span></a>. All rights reserved.
+                &copy; {new Date().getFullYear()} <a href="https://github.com/leon0113" target='_blank'><span className='font-bold text-base text-violet-500 opacity-100 hover:text-violet-600'>leon0113</span></a>. All rights reserved.
             </small>
         </footer>
     )
